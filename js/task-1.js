e9ea6beb6026eeea83d1ffa65f31c16c96f07c94
Taras1000 <semkivtaras80@gmail.com>
2023-12-03 20:01:58 +0000

    done

---
+++ js/task-1.js
@@ -0,0 +1,28 @@
+`use strict`;
+function isEnoughCapacity(products, containerSize) {
+
+    let totalQuantity = 0;
+    const productValue = Object.values(products);
+
+    for (const quantity of productValue) {
+        totalQuantity += quantity;
+    }
+    const result = (totalQuantity <= containerSize) ? true : false;
+    return result;
+}
+
+console.log(
+    isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
+
+console.log(
+    isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
+
+console.log(
+    isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+); // true
+
+console.log(
+    isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+); // false
